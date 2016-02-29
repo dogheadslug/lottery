dd98b2ac5b71636c0ab29e1c8fab6cc09814f252
dogheadslug <zhon4@ucsc.edu>
2016-02-28 16:54:34 -0800

    Create LotteryUI.java

---
+++ LotteryUI.java
@@ -0,0 +1,28 @@
+import java.awt.Container;
+import java.awt.GridLayout;
+import javax.swing.*;
+
+class LotteryUI {
+public static void main(String[] args) {
+   JFrame frame = new JFrame("吃饭喝粥拼一枪");
+   Container pane = frame.getContentPane();
+   
+   //create components
+   JTextField result = new JTextField(20);
+   JButton go = new JButton("GO");
+   
+   //layout add components
+   pane.setLayout(new GridLayout(1, 3));
+   
+   //add all the components
+   pane.add(go);
+   //pane.add(new JLabel("Result:"));
+   pane.add(result);
+   
+   //listener
+   lottery listener = new lottery(result);
+   go.addActionListener(listener);
+   frame.pack();
+   frame.setVisible(true);
+   }
+}
